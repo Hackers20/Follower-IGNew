9a072372699329d485f0cd367a8a0939cc8d4d35
Hackers20 <70922207+Hackers20@users.noreply.github.com>
2020-10-04 16:39:27 +0700

    Followers IG Free

---
+++ IG.sh
@@ -0,0 +1,56 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'                                    purple='\033[35;1m'                                   cyan='\033[36;1m'
+red='\033[31;1m'                                      white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+figlet FOLLOWER IG | lolcat
+sleep 1
+echo
+echo "<================================================>" | lolcat
+echo "<==========[    Tools By Hacker2    ]==========>" | lolcat
+echo "<=========[ Contact Me:6287778506999 ]==========>" | lolcat
+echo "<================================================>" | lolcat
+sleep 1
+echo $green"Login Akun IG [+]"
+echo
+sleep 1
+echo -n "Ussername : ";read IS
+echo
+sleep 1
+echo -n "Password : ";read PW
+echo
+echo
+sleep 1
+echo $green"Tenang Ini Cuma Login Doang Akun Nya Nggak Di Hack"
+echo
+sleep 1
+echo -n $cayn"Berapa Follower : ";read Fw
+sleep 2
+echo
+echo $blue "Mengecek>>"
+echo
+sleep 2
+echo "-------------------------------" | lolcat
+echo $green"Ussername = "$IS
+echo $green"Password  = "$PW
+echo $green"Berapa Follow = "$Fw
+echo "-------------------------------" | lolcat
+sleep 2
+echo
+echo
+figlet SUKSES | lolcat
+echo
+echo
+sleep 3
+echo "Proses Keluar" | lolcat
+echo
+sleep 3
+clear
+figlet DONE | lolcat
+echo
+echo "==========================================="
+echo "Silahkan Tunggu 10 Menit" | lolcat
+echo "Untuk Proses Follower" | lolcat
+echo "Jika Script Rusak Silahkan Hubungi ADMIN" | lolcat
+echo "===========================================" | lolcat
